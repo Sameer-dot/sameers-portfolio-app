a8846b6bc4797e8e6024db5b2ae51a8e5e333339
sameer-dot <sameernaveed37@gmail.com>
2026-02-23 01:26:53 +0500

    Add force-exit plugin to Vite configuration for graceful shutdown

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'force-exit',
+      closeBundle() {
+        setTimeout(() => process.exit(0), 2000);
+      },
+    },
+  ],
 })
